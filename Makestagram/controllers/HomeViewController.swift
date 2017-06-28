2ee55c94ccc6137df1a3d90d161a71e54c6e95a4
Chau Tung Lam <nctl144@gmail.com>
2017-06-28 10:20:08 -0700

    create the sections for cell, display them on the timeline

--- Makestagram/controllers/HomeViewController.swift
+++ Makestagram/controllers/HomeViewController.swift
@@ -41,23 +41,52 @@ class HomeViewController: UIViewController {
 
 extension HomeViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return posts.count
+        return 3
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PostImageCell", for: indexPath) as! PostImageCell
-        let post = posts[indexPath.row]
-        let imageUrl = URL(string: post.imageUrl)
         
-        cell.postImageView.kf.setImage(with: imageUrl)
+        let post = posts[indexPath.section]
         
-        return cell
+        switch indexPath.row {
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PostHeaderCell") as! PostHeaderCell
+            cell.usernameLabel.text = User.current.username
+    
+            return cell
+        case 1:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PostImageCell") as! PostImageCell
+            let imageUrl = URL(string: post.imageUrl)
+            
+            cell.postImageView.kf.setImage(with: imageUrl)
+            return cell
+        case 2:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PostActionCell") as! PostActionCell
+            
+            return cell
+        default:
+            fatalError("Error: unexpected indexPath.")
+        }
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return posts.count
     }
 }
 
 extension HomeViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        let post = posts[indexPath.row]
-        return post.imageHeight
+        switch indexPath.row {
+        case 0:
+            return PostHeaderCell.height
+        case 1:
+            let post = posts[indexPath.section]
+            return post.imageHeight
+        case 2:
+            return PostActionCell.height
+        default:
+            fatalError()
+        }
     }
 }
---
+++ Makestagram/views/PostActionCell.swift
@@ -0,0 +1,18 @@
+//
+//  PostActionCell.swift
+//  Makestagram
+//
+//  Created by Nguyễn Lâm on 6/28/17.
+//  Copyright © 2017 Make School. All rights reserved.
+//
+
+import UIKit
+
+class PostActionCell: UITableViewCell {
+
+    static let height: CGFloat = 46
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
